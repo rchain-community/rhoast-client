5602de4daf9001f306cfc10aa62cc0e2b844a965
Valentine-Mario <valentine13400@gmail.com>
2022-11-01 11:55:24 +0100

    refactor and example usage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 mod models;
+mod proto;
 mod utils;
+// use crate::proto::{casper, casper::casper_v1, routing, scalapb};
+use crate::proto::{casper::casper_v1::*, routing::*, casper::*, scalapb::*};
 
 fn main() {
-    println!("hello world!")
+    println!("hello world!");
+    ServiceError{
+        messages:vec!["hello".to_string()]
+    };
+    HeartbeatResponse{};
+    PCost{
+        cost:34
+    };
 }
