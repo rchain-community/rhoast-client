b563d3467856364eeed974b6ba7af8e5550f7cf4
Valentine-Mario <valentine13400@gmail.com>
2022-11-02 10:05:55 +0100

    added blakeeb

---
+++ src/utils/getBlake2Hash.rs
@@ -0,0 +1,12 @@
+use blake2::digest::{Update, VariableOutput};
+use blake2::Blake2bVar;
+
+pub fn getBlake2Hash(toHash: &[u8], length: Option<u32>) -> Vec<u8> {
+    let len = length.unwrap_or(32);
+    let mut hasher = Blake2bVar::new(len as usize).unwrap();
+    hasher.update(toHash);
+    //write hash to buffer
+    let mut buf: Vec<u8> = Vec::with_capacity(len as usize);
+    hasher.finalize_variable(&mut buf).unwrap();
+    buf
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,3 @@
 pub mod base58;
+pub mod getBlake2Hash;
 pub const Sig_Algorithm: &str = "secp256k1";
