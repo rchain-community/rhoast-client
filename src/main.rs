d3847935aaca811435d0456d79b85f977f3b63c5
Valentine-Mario <valentine13400@gmail.com>
2022-11-02 10:59:26 +0100

    removed unnecessary comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod models;
 mod proto;
 mod utils;
-// use crate::proto::{casper, casper::casper_v1, routing, scalapb};
 use crate::proto::{
     casper::casper_v1::*, casper::*, casper_msg::*, deploy::*, deployv1::ServiceError, routing::*,
     scalapb::*,
