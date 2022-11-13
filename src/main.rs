ec7b2e63dcb560110b7ac8421cb955a8d1905d8b
Valentine-Mario <valentine13400@gmail.com>
2022-11-13 11:09:05 +0100

    added dummy test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ mod models;
 mod proto;
 mod utils;
 
+use crate::example::run;
+
 fn main() {
     println!("hello world!");
+    run();
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -45,6 +45,6 @@ mod tests {
     #[test]
     fn it_works() {
         let result = 2 + 2;
-        assert_eq!(result, 5);
+        assert_eq!(result, 4);
     }
 }
