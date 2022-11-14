b7a6c305448476f43f7aa0ece06aba96e1582e15
Valentine-Mario <valentine13400@gmail.com>
2022-11-14 09:18:15 +0100

    added more explicit error message

--- src/utils/base58.rs
+++ src/utils/base58.rs
@@ -3,8 +3,15 @@ use crate::error::ErrCode;
 pub fn decode(input: &str) -> Result<Vec<u8>, ErrCode> {
     match bs58::decode(input).into_vec() {
         Ok(decoded) => Ok(decoded),
-        Err(_) => Err(ErrCode::Bs58("Error decoding to vec")),
+        Err(_) => {
+            let err_msg = format!("Error decoding {} to vec", input);
+            Err(ErrCode::Bs58(string_to_static_str(err_msg)))
+        }
     }
+}
+
+fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
 }
 
 pub fn encode(input: Vec<u8>) -> String {
