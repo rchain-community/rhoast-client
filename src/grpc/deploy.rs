1e4589b9b0a23c5ffe9739219808db604c32e8ee
Valentine-Mario <valentine13400@gmail.com>
2022-12-14 11:26:53 +0100

    added stream

--- src/grpc/deploy.rs
+++ src/grpc/deploy.rs
@@ -3,17 +3,18 @@ use crate::proto::casper_msg::*;
 use crate::proto::deploy::*;
 use crate::proto::deployv1::*;
 use crate::utils::base58::string_to_static_str;
+use futures::StreamExt;
 use tonic::Request;
 
 pub async fn do_deploy_util(
     host: String,
     payload: DeployDataProto,
-) -> Result<tonic::Response<DeployResponse>, ErrCode> {
+) -> Result<DeployResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.do_deploy(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -27,15 +28,12 @@ pub async fn do_deploy_util(
     }
 }
 
-pub async fn get_block_util(
-    host: String,
-    payload: BlockQuery,
-) -> Result<tonic::Response<BlockResponse>, ErrCode> {
+pub async fn get_block_util(host: String, payload: BlockQuery) -> Result<BlockResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.get_block(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -52,12 +50,12 @@ pub async fn get_block_util(
 pub async fn machine_verification_dag_util(
     host: String,
     payload: MachineVerifyQuery,
-) -> Result<tonic::Response<MachineVerifyResponse>, ErrCode> {
+) -> Result<MachineVerifyResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.machine_verifiable_dag(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -74,12 +72,12 @@ pub async fn machine_verification_dag_util(
 pub async fn listen_for_date_at_name_util(
     host: String,
     payload: DataAtNameQuery,
-) -> Result<tonic::Response<ListeningNameDataResponse>, ErrCode> {
+) -> Result<ListeningNameDataResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.listen_for_data_at_name(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -96,12 +94,12 @@ pub async fn listen_for_date_at_name_util(
 pub async fn listen_for_continuation_at_name_util(
     host: String,
     payload: ContinuationAtNameQuery,
-) -> Result<tonic::Response<ContinuationAtNameResponse>, ErrCode> {
+) -> Result<ContinuationAtNameResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.listen_for_continuation_at_name(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -118,12 +116,12 @@ pub async fn listen_for_continuation_at_name_util(
 pub async fn find_deploy_util(
     host: String,
     payload: FindDeployQuery,
-) -> Result<tonic::Response<FindDeployResponse>, ErrCode> {
+) -> Result<FindDeployResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.find_deploy(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -140,12 +138,12 @@ pub async fn find_deploy_util(
 pub async fn prview_private_names_util(
     host: String,
     payload: PrivateNamePreviewQuery,
-) -> Result<tonic::Response<PrivateNamePreviewResponse>, ErrCode> {
+) -> Result<PrivateNamePreviewResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.preview_private_names(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -162,12 +160,12 @@ pub async fn prview_private_names_util(
 pub async fn last_finalized_block_util(
     host: String,
     payload: LastFinalizedBlockQuery,
-) -> Result<tonic::Response<LastFinalizedBlockResponse>, ErrCode> {
+) -> Result<LastFinalizedBlockResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.last_finalized_block(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -184,12 +182,12 @@ pub async fn last_finalized_block_util(
 pub async fn is_finalized_util(
     host: String,
     payload: IsFinalizedQuery,
-) -> Result<tonic::Response<IsFinalizedResponse>, ErrCode> {
+) -> Result<IsFinalizedResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.is_finalized(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -206,12 +204,12 @@ pub async fn is_finalized_util(
 pub async fn bond_status_util(
     host: String,
     payload: BondStatusQuery,
-) -> Result<tonic::Response<BondStatusResponse>, ErrCode> {
+) -> Result<BondStatusResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.bond_status(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -228,12 +226,12 @@ pub async fn bond_status_util(
 pub async fn exploratory_deploy_util(
     host: String,
     payload: ExploratoryDeployQuery,
-) -> Result<tonic::Response<ExploratoryDeployResponse>, ErrCode> {
+) -> Result<ExploratoryDeployResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.exploratory_deploy(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
@@ -250,14 +248,130 @@ pub async fn exploratory_deploy_util(
 pub async fn get_event_by_hash_util(
     host: String,
     payload: BlockQuery,
-) -> Result<tonic::Response<EventInfoResponse>, ErrCode> {
+) -> Result<EventInfoResponse, ErrCode> {
     match deploy_service_client::DeployServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(payload);
             match client.get_event_by_hash(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
+                    Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+                }
+            }
+        }
+        Err(err) => {
+            let err = format!("Error creating GRPC connection {}", err);
+            Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+        }
+    }
+}
+
+pub async fn visualize_dag_util_stream(
+    host: String,
+    payload: VisualizeDagQuery,
+    output: &mut Vec<VisualizeBlocksResponse>,
+) -> Result<(), ErrCode> {
+    match deploy_service_client::DeployServiceClient::connect(host).await {
+        Ok(mut client) => {
+            let request = Request::new(payload);
+            match client.visualize_dag(request).await {
+                Ok(stream) => {
+                    let mut stream = stream.into_inner();
+                    while let Some(item) = stream.next().await {
+                        output.push(item.unwrap())
+                    }
+                    Ok(())
+                }
+                Err(err) => {
+                    let err = format!("Error getting stream {}", err);
+                    Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+                }
+            }
+        }
+        Err(err) => {
+            let err = format!("Error creating GRPC connection {}", err);
+            Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+        }
+    }
+}
+
+pub async fn show_main_chain_util_stream(
+    host: String,
+    payload: BlocksQuery,
+    output: &mut Vec<BlockInfoResponse>,
+) -> Result<(), ErrCode> {
+    match deploy_service_client::DeployServiceClient::connect(host).await {
+        Ok(mut client) => {
+            let request = Request::new(payload);
+            match client.show_main_chain(request).await {
+                Ok(stream) => {
+                    let mut stream = stream.into_inner();
+                    while let Some(item) = stream.next().await {
+                        output.push(item.unwrap())
+                    }
+                    Ok(())
+                }
+                Err(err) => {
+                    let err = format!("Error getting stream {}", err);
+                    Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+                }
+            }
+        }
+        Err(err) => {
+            let err = format!("Error creating GRPC connection {}", err);
+            Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+        }
+    }
+}
+
+pub async fn show_blocks_util_stream(
+    host: String,
+    payload: BlocksQuery,
+    output: &mut Vec<BlockInfoResponse>,
+) -> Result<(), ErrCode> {
+    match deploy_service_client::DeployServiceClient::connect(host).await {
+        Ok(mut client) => {
+            let request = Request::new(payload);
+            match client.get_blocks(request).await {
+                Ok(stream) => {
+                    let mut stream = stream.into_inner();
+                    while let Some(item) = stream.next().await {
+                        output.push(item.unwrap())
+                    }
+                    Ok(())
+                }
+                Err(err) => {
+                    let err = format!("Error getting stream {}", err);
+                    Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+                }
+            }
+        }
+        Err(err) => {
+            let err = format!("Error creating GRPC connection {}", err);
+            Err(ErrCode::GrpcUtil(string_to_static_str(err)))
+        }
+    }
+}
+
+pub async fn get_blocks_by_height_util_stream(
+    host: String,
+    payload: BlocksQueryByHeight,
+    output: &mut Vec<BlockInfoResponse>,
+) -> Result<(), ErrCode> {
+    match deploy_service_client::DeployServiceClient::connect(host).await {
+        Ok(mut client) => {
+            let request = Request::new(payload);
+            match client.get_blocks_by_heights(request).await {
+                Ok(stream) => {
+                    let mut stream = stream.into_inner();
+                    while let Some(item) = stream.next().await {
+                        output.push(item.unwrap())
+                    }
+                    Ok(())
+                }
+                Err(err) => {
+                    let err = format!("Error getting stream {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
                 }
             }
--- src/grpc/propose.rs
+++ src/grpc/propose.rs
@@ -3,17 +3,14 @@ use crate::proto::casper::casper_v1::*;
 use crate::utils::base58::string_to_static_str;
 use tonic::Request;
 
-pub async fn propose_util(
-    host: String,
-    payload: bool,
-) -> Result<tonic::Response<ProposeResponse>, ErrCode> {
+pub async fn propose_util(host: String, payload: bool) -> Result<ProposeResponse, ErrCode> {
     match propose_service_client::ProposeServiceClient::connect(host).await {
         Ok(mut client) => {
             let request = Request::new(PrintUnmatchedSendsQuery {
                 print_unmatched_sends: payload,
             });
             match client.propose(request).await {
-                Ok(respose) => Ok(respose),
+                Ok(respose) => Ok(respose.into_inner()),
                 Err(err) => {
                     let err = format!("Error getting response {}", err);
                     Err(ErrCode::GrpcUtil(string_to_static_str(err)))
