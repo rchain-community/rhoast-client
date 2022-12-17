1fdbda21f9358116b327fa2891f2db37687cce56
Kunj <bill.keuntje@gmail.com>
2022-12-17 16:37:18 -0500

    add config step

--- src/examples/transfer.rs
+++ src/examples/transfer.rs
@@ -5,6 +5,7 @@ pub fn transfer(from: String, to: String, amount: uint64) {
    // get first line of rholang
    // parse out what's in the [square brackets]
    // replace from left to right with arguments passed in, checking for type on the way
+   // read or create key configuration
    // invoke deploy/propose or easyDeploy
    // print response
 }
