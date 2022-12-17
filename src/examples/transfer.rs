88a2e8c38d08095aa0e703858c57b2a90cd2809e
Kunj <bill.keuntje@gmail.com>
2022-12-17 16:29:49 -0500

    prototype transfer example

---
+++ src/examples/transfer.rs
@@ -0,0 +1,10 @@
+mod examples;
+
+pub fn transfer(from: String, to: String, amount: uint64) {
+   // read file transfer.rho
+   // get first line of rholang
+   // parse out what's in the [square brackets]
+   // replace from left to right with arguments passed in, checking for type on the way
+   // invoke deploy/propose or easyDeploy
+   // print response
+}
