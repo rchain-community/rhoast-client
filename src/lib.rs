e1c317202018e9d2103edf8ee6e378f578fd8926
Valentine-Mario <valentine13400@gmail.com>
2023-01-09 10:31:53 +0100

    used lib instead of main

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod config;
+pub mod error;
+//TODO: this example should be made private before publishing
+pub mod example;
+pub mod grpc;
+pub mod http;
+pub mod models;
+mod proto;
+pub mod utils;
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-mod config;
-mod error;
-mod example;
-mod grpc;
-mod http;
-mod models;
-mod proto;
-mod utils;
-
-use crate::example::run;
+use rust_client::example::run;
 fn main() {
     println!("hello world!");
     run();
