9aaed4fdcd00cdac9f2960fbe7aafd04bc0e0741
Kunj <bill.keuntje@gmail.com>
2023-01-12 16:17:58 -0500

    add updated lib.rs for reorganization

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-pub mod config;
 pub mod error;
 //TODO: this example should be made private before publishing
 pub mod example;
-pub mod grpc;
-pub mod http;
 pub mod models;
 mod proto;
-pub mod utils;
