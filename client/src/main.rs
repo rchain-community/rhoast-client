4e499f7dc1779ac88dd10da5cc21c608fb4c6fe5
Valentine-Mario <valentine13400@gmail.com>
2023-02-17 09:35:02 +0100

    format

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,7 +7,6 @@ async fn main() {
     // let data = block_call(&"http://rnode-06.test.r-pub.com:40403".to_string(), opt).await;
     // println!("{:?}", data);
 
-    
-    let status=status(&"http://rnode-06.test.r-pub.com:40403".to_string()).await;
+    let status = status(&"http://rnode-06.test.r-pub.com:40403".to_string()).await;
     println!("{:?}", status);
 }
