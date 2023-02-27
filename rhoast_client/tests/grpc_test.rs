4d2a665b73b5cd0543dd333556eaf6709d009f54
Valentine-Mario <valentine13400@gmail.com>
2023-02-27 09:15:46 +0100

    reconfigured test to read env variables

--- rhoast_client/tests/grpc_test.rs
+++ rhoast_client/tests/grpc_test.rs
@@ -1,11 +1,41 @@
+use futures::Future;
 use rhoast_client::grpc::deploy::*;
 use rhoast_client::grpc::propose::propose_util;
 use rhoast_client::proto::casper::*;
 
-const ENDPOINT: &str = "http://167.235.8.107:40401";
-const PROPOSE_ENDPOINT: &str = "http://167.235.8.107:40402";
+const OTHER_PORT: &str = "40401";
+const PROPOSE_PORT: &str = "40402";
+const KEY: &str = "URL";
+
+async fn check_env<F, Fut>(input: F)
+where
+    F: FnOnce() -> Fut,
+    Fut: Future<Output = ()>,
+{
+    let value = std::env::var(KEY);
+    match value {
+        Ok(_url) => input().await,
+        Err(_e) => {
+            println!("no url provided, skipping test")
+        }
+    }
+}
 
 #[tokio::test]
+async fn exec_all_test() {
+    check_env(test_do_deploy_util).await;
+    check_env(test_get_block_util).await;
+    check_env(test_machine_verification_dag_util).await;
+    check_env(test_find_deploy_util).await;
+    check_env(test_prview_private_names_util).await;
+    check_env(test_last_finalized_block_util).await;
+    check_env(test_is_finalized_util).await;
+    check_env(test_bond_status_util).await;
+    check_env(test_exploratory_deploy_util).await;
+    check_env(test_get_event_by_hash_util).await;
+    check_env(test_prpopose_util).await;
+}
+
 async fn test_do_deploy_util() {
     let payload = DeployDataProto {
         deployer: "Q2FsaWZvcm5pYSBQbGFzdGlj".as_bytes().to_vec(),
@@ -17,112 +47,115 @@ async fn test_do_deploy_util() {
         phlo_limit: 968,
         valid_after_block_number: 446,
     };
-    let deploy = do_deploy_util(ENDPOINT.to_string(), payload).await.unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+    let deploy = do_deploy_util(url_port, payload).await.unwrap();
     assert!(deploy.message.is_some());
 }
 
-#[tokio::test]
 async fn test_get_block_util() {
     let payload = BlockQuery {
         hash: "hash_value".to_string(),
     };
-    let block = get_block_util(ENDPOINT.to_string(), payload).await.unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+    let block = get_block_util(url_port, payload).await.unwrap();
     assert!(block.message.is_some());
 }
 
-#[tokio::test]
 async fn test_machine_verification_dag_util() {
     let payload = MachineVerifyQuery {};
-    let dag = machine_verification_dag_util(ENDPOINT.to_string(), payload)
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+    let dag = machine_verification_dag_util(url_port, payload)
         .await
         .unwrap();
     assert!(dag.message.is_some());
 }
 
-#[tokio::test]
 async fn test_find_deploy_util() {
     let payload = FindDeployQuery {
         deploy_id: "Q2FsaWZvcm5pYSBQbGFzdGlj".as_bytes().to_vec(),
     };
-    let deploy = find_deploy_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let deploy = find_deploy_util(url_port, payload).await.unwrap();
     assert!(deploy.message.is_some());
 }
 
-#[tokio::test]
 async fn test_prview_private_names_util() {
     let payload = PrivateNamePreviewQuery {
         user: "Q2FsaWZvcm5pYSBQbGFzdGlj".as_bytes().to_vec(),
         timestamp: 148,
         name_qty: 164,
     };
-    let ids = prview_private_names_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+    let ids = prview_private_names_util(url_port, payload).await.unwrap();
     assert!(ids.message.is_some());
 }
 
-#[tokio::test]
 async fn test_last_finalized_block_util() {
     let payload = LastFinalizedBlockQuery {};
-    let block = last_finalized_block_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let block = last_finalized_block_util(url_port, payload).await.unwrap();
     assert!(block.message.is_some());
 }
 
-#[tokio::test]
 async fn test_is_finalized_util() {
     let payload = IsFinalizedQuery {
         hash: "hash".to_string(),
     };
-    let finalized = is_finalized_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let finalized = is_finalized_util(url_port, payload).await.unwrap();
     assert!(finalized.message.is_some());
 }
 
-#[tokio::test]
 async fn test_bond_status_util() {
     let payload = BondStatusQuery {
         public_key: "key".as_bytes().to_vec(),
     };
-    let bond = bond_status_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let bond = bond_status_util(url_port, payload).await.unwrap();
     assert!(bond.message.is_some());
 }
 
-#[tokio::test]
 async fn test_exploratory_deploy_util() {
     let payload = ExploratoryDeployQuery {
         term: "term".to_string(),
         block_hash: "hash".to_string(),
         use_pre_state_hash: false,
     };
-    let explore = exploratory_deploy_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let explore = exploratory_deploy_util(url_port, payload).await.unwrap();
     assert!(explore.message.is_some());
 }
 
-#[tokio::test]
 async fn test_get_event_by_hash_util() {
     let payload = BlockQuery {
         hash: "hash".to_string(),
     };
-    let explore = get_event_by_hash_util(ENDPOINT.to_string(), payload)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{OTHER_PORT}");
+
+    let explore = get_event_by_hash_util(url_port, payload).await.unwrap();
     assert!(explore.message.is_some());
 }
 
-#[tokio::test]
 async fn test_prpopose_util() {
-    let propose = propose_util(PROPOSE_ENDPOINT.to_string(), true)
-        .await
-        .unwrap();
+    let url = std::env::var(KEY).unwrap();
+    let url_port = format!("{url}:{PROPOSE_PORT}");
+
+    let propose = propose_util(url_port, true).await.unwrap();
 
     assert!(propose.message.is_some())
 }
