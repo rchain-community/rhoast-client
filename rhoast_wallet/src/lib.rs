35d2fe35f9d29cc529ac5f344a5cd777ea2a61d0
Valentine-Mario <valentine13400@gmail.com>
2023-03-13 11:47:52 +0100

    added struct payload

--- rhoast_wallet/src/lib.rs
+++ rhoast_wallet/src/lib.rs
@@ -1,1 +1,18 @@
 pub mod rho;
+
+#[derive(Debug)]
+pub struct Node {
+    pub http_url: String,
+    pub grpc_url: String,
+    pub shard_id: String,
+}
+
+#[derive(Debug)]
+pub struct Account {
+    pub from_acc_name: String,
+    pub from_acc_addr: String,
+    pub to_acc_name: String,
+    pub to_acc_addr: String,
+    pub pri_key: String,
+    pub amount: u64,
+}
