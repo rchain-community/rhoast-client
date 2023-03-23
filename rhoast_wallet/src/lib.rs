af421e7d2aa755a2ea51b1f260f6de9101e53742
Valentine-Mario <valentine13400@gmail.com>
2023-03-23 12:40:56 +0100

    aded check balance

--- rhoast_wallet/src/lib.rs
+++ rhoast_wallet/src/lib.rs
@@ -23,6 +23,10 @@ pub struct Account {
     pub amount: u64,
 }
 
+fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
+}
+
 impl Node {
     pub fn new(
         http_url: String,
@@ -40,13 +44,56 @@ impl Node {
         }
     }
 
-    pub async fn check_balance(&self, rev_addr: String) -> Result<(), Error> {
+    pub async fn check_balance(&self, rev_addr: String) -> Result<Option<u64>, Error> {
         let term = rho::check_balance(rev_addr);
         let payload = ExploreDataOptions { term };
         let res = explore_deploy(&self.http_url, payload).await;
 
         match res {
-            Ok(response) => Ok(()),
+            Ok(response) => {
+                let value: std::result::Result<serde_json::Value, serde_json::Error> =
+                    serde_json::from_str(&response);
+                match value {
+                    Ok(json) => {
+                        let expr = &json.get("expr");
+                        match expr {
+                            Some(expr_val) => {
+                                let expr_index = &expr_val[0];
+                                let expr_int = expr_index.get("ExprInt");
+                                match expr_int {
+                                    Some(expr_int) => {
+                                        let data = expr_int.get("data");
+                                        match data {
+                                            Some(data) => Ok(data.as_u64()),
+                                            None => {
+                                                let err = format!(
+                                                    "Error getting balance {:?}",
+                                                    expr_val["ExprString"]["data"]
+                                                );
+                                                Err(Error::CheckBlance(string_to_static_str(err)))
+                                            }
+                                        }
+                                    }
+                                    None => {
+                                        let err = format!(
+                                            "Error getting balance {:?}",
+                                            expr_val["ExprString"]["data"]
+                                        );
+                                        Err(Error::CheckBlance(string_to_static_str(err)))
+                                    }
+                                }
+                            }
+                            None => Err(Error::CheckBlance("No expresssion found")),
+                        }
+                    }
+                    Err(error) => {
+                        let err = format!("Error parsing json {}", error);
+                        Err(Error::CheckBlance(
+                            rhoast_client::http::string_to_static_str(err),
+                        ))
+                    }
+                }
+            }
             Err(err) => {
                 let err = format!("Error getting balance {}", err);
                 Err(Error::CheckBlance(
