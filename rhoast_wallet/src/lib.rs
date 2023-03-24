f162ab99746afe0627bb40e66252fcd1d412282f
Valentine-Mario <valentine13400@gmail.com>
2023-03-24 12:17:08 +0100

    added func to verify addr

--- rhoast_wallet/src/lib.rs
+++ rhoast_wallet/src/lib.rs
@@ -3,6 +3,7 @@ pub mod rho;
 
 use error::Error;
 use rhoast_client::{http::explore::explore_deploy, models::model::ExploreDataOptions};
+use rhoast_utils::rev_address_from_public_key::verify_rev_addr;
 
 #[derive(Debug)]
 pub struct Node {
@@ -45,26 +46,43 @@ impl Node {
     }
 
     pub async fn check_balance(&self, rev_addr: String) -> Result<Option<u64>, Error> {
-        let term = rho::check_balance(rev_addr);
-        let payload = ExploreDataOptions { term };
-        let res = explore_deploy(&self.http_url, payload).await;
+        match verify_rev_addr(&rev_addr) {
+            Ok(addr) => {
+                if !addr {
+                    return Err(Error::CheckBlance("invalid rev addr"));
+                } else {
+                }
+                let term = rho::check_balance(rev_addr);
+                let payload = ExploreDataOptions { term };
+                let res = explore_deploy(&self.http_url, payload).await;
 
-        match res {
-            Ok(response) => {
-                let value: std::result::Result<serde_json::Value, serde_json::Error> =
-                    serde_json::from_str(&response);
-                match value {
-                    Ok(json) => {
-                        let expr = &json.get("expr");
-                        match expr {
-                            Some(expr_val) => {
-                                let expr_index = &expr_val[0];
-                                let expr_int = expr_index.get("ExprInt");
-                                match expr_int {
-                                    Some(expr_int) => {
-                                        let data = expr_int.get("data");
-                                        match data {
-                                            Some(data) => Ok(data.as_u64()),
+                match res {
+                    Ok(response) => {
+                        let value: std::result::Result<serde_json::Value, serde_json::Error> =
+                            serde_json::from_str(&response);
+                        match value {
+                            Ok(json) => {
+                                let expr = &json.get("expr");
+                                match expr {
+                                    Some(expr_val) => {
+                                        let expr_index = &expr_val[0];
+                                        let expr_int = expr_index.get("ExprInt");
+                                        match expr_int {
+                                            Some(expr_int) => {
+                                                let data = expr_int.get("data");
+                                                match data {
+                                                    Some(data) => Ok(data.as_u64()),
+                                                    None => {
+                                                        let err = format!(
+                                                            "Error getting balance {:?}",
+                                                            expr_val["ExprString"]["data"]
+                                                        );
+                                                        Err(Error::CheckBlance(
+                                                            string_to_static_str(err),
+                                                        ))
+                                                    }
+                                                }
+                                            }
                                             None => {
                                                 let err = format!(
                                                     "Error getting balance {:?}",
@@ -74,32 +92,26 @@ impl Node {
                                             }
                                         }
                                     }
-                                    None => {
-                                        let err = format!(
-                                            "Error getting balance {:?}",
-                                            expr_val["ExprString"]["data"]
-                                        );
-                                        Err(Error::CheckBlance(string_to_static_str(err)))
-                                    }
+                                    None => Err(Error::CheckBlance("No expresssion found")),
                                 }
                             }
-                            None => Err(Error::CheckBlance("No expresssion found")),
+                            Err(error) => {
+                                let err = format!("Error parsing json {}", error);
+                                Err(Error::CheckBlance(
+                                    rhoast_client::http::string_to_static_str(err),
+                                ))
+                            }
                         }
                     }
-                    Err(error) => {
-                        let err = format!("Error parsing json {}", error);
+                    Err(err) => {
+                        let err = format!("Error getting balance {}", err);
                         Err(Error::CheckBlance(
                             rhoast_client::http::string_to_static_str(err),
                         ))
                     }
                 }
             }
-            Err(err) => {
-                let err = format!("Error getting balance {}", err);
-                Err(Error::CheckBlance(
-                    rhoast_client::http::string_to_static_str(err),
-                ))
-            }
+            Err(_) => Err(Error::CheckBlance("Error verifying rev address")),
         }
     }
 }
