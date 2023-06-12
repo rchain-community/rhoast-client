cf950959008fe22b45853a6e90e5e498af0a54ff
Kunj <bill.keuntje@gmail.com>
2023-06-12 17:44:43 -0400

    Add multi-arg rhofunc code

--- rhoast_functions/src/rholang/mod.rs
+++ rhoast_functions/src/rholang/mod.rs
@@ -1,12 +1,27 @@
 use handlebars::Handlebars;
 use serde_json::json;
-use std::{error::Error, fs};
+use std::{error::Error, fs, collections::BTreeMap};
 
 pub fn parse_simple_deploy(text: &String) -> Result<String, Box<dyn Error>> {
     let reg = Handlebars::new();
     let rholang_code = get_rholang("simple_rholang.txt");
     let compiled = reg.render_template(&rholang_code, &json!({ "text": &text }))?;
     Ok(compiled)
+}
+
+fn parse_simple(rholang: &String, arg: &String, text: &String) -> Result<String, Box<dyn Error>> {
+    let reg = Handlebars::new();
+    let compiled = reg.render_template(&rholang, &json!({ arg: &text }))?;
+    Ok(compiled)
+}
+
+pub fn parse_multiple_args(rholang: &String, args: &BTreeMap<String,String>) -> Result<String,Box<dyn Error>> {
+    let mut rtc: String = rholang.to_string();
+    for a in args.keys() {
+        let val = args.get(a).unwrap();
+        rtc = parse_simple(&rtc, a, val).unwrap().to_string();
+    }
+    Ok(rtc.to_string())
 }
 
 pub fn get_rholang(path: &str) -> String {
